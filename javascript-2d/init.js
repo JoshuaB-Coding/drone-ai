6e1305839c90187e8a050cfca00d6eca6dec86c0
Joshua Binns <joshuabinns.coding@gmail.com>
2023-03-29 10:49:37 +0100

    Implemented simple background functionality

--- javascript-2d/init.js
+++ javascript-2d/init.js
@@ -27,17 +27,39 @@ function startGame() {
             drone.reset();
         }
 
+        // Render background first to push it to back
+        domain.renderBackground(drone);
         drone.render(domain.context);
     }, dt * 1000);
 }
   
 var domain = {
     canvas: document.createElement("canvas"),
+    background_image: new Image(),
     setup: function(parent) {
         this.canvas.width = CANVAS_WIDTH;
         this.canvas.height = CANVAS_HEIGHT;
         this.context = this.canvas.getContext("2d");
         parent.appendChild(this.canvas);
+
+        this.background_image.src = "background_1.png";
+    },
+    renderBackground(drone) {
+        // Currently doesn't scale with grid size - need to fix this
+        const X = [-CANVAS_WIDTH, 0, CANVAS_WIDTH];
+        const Y = [-CANVAS_HEIGHT, 0, CANVAS_HEIGHT];
+
+        // Adding drone position
+        for (let i = 0; i < 3; i++) {
+            X[i] += (drone.x - CANVAS_WIDTH / 2) * 2;
+            Y[i] -= (drone.y - CANVAS_HEIGHT / 2) * 2;
+        }
+
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                this.context.drawImage(this.background_image, X[i], Y[j], CANVAS_WIDTH, CANVAS_HEIGHT);
+            }
+        }
     },
     registerKeyDown: function(e, drone) {
         switch(e.code) {
