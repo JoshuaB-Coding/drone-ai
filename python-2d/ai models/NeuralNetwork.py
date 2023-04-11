ab385eccca74f72fec4fb4c8609ed63a40d48a72
Joshua Binns <joshuabinns.coding@gmail.com>
2023-04-11 15:45:03 +0100

    First few files

---
+++ python-2d/ai models/NeuralNetwork.py
@@ -0,0 +1,14 @@
+
+
+class NeuralNetwork:
+    def __init__(self):
+        pass
+
+class Layers:
+    def __init__(self):
+        pass
+
+class Layer:
+    def __init__(self, **kwargs):
+        self.numberOfInputs = kwargs['numberOfInputs']
+        self.numberOfOutputs = kwargs['numberOfOutputs']
